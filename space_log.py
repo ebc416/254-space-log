9ae76f56bd01153b75d7a68457160a2d294fecda
Ebc416 <efrainbonilla22@csu.fullerton.edu>
2018-03-31 01:37:56 -0700

    fixed print to show without quotes

--- space_log.py
+++ space_log.py
@@ -17,7 +17,7 @@ except:
 
 # Uncomment and your work in the appropriate spots.
 def Names_of_system_visted(content):
-    pattern = re.compile(r"\"event\":\"FSDJump\", \"StarSystem\":\"(\w+\s\w+[-\s]\w+[-\s]\w+[-\s\w]\"?\w?\w?\w?\w?\s?\w?-?\w?-?\w?\w?\"?)")  
+    pattern = re.compile(r"\"event\":\"FSDJump\", \"StarSystem\":\"(\w+\s\w+[-\s]\w+[-\s]\w+[-\s\w]\"?\w?\w?\w?\w?\s?\w?-?\w?-?\w?\w?)\"?")  
     matches = pattern.finditer(content)
     for match in matches:
         print(match.group(1))
