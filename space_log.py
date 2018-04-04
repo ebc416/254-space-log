da182512e55a36e32faf3eaf07bcab24a7dcab1e
Ebc416 <efrainbonilla22@csu.fullerton.edu>
2018-04-03 21:43:12 -0700

    Adding own planet_list.py

--- space_log.py
+++ space_log.py
@@ -3,7 +3,7 @@
 # Use like `./space_log.py -s|-p|-t|-d|-f log_file
 
 from sys import argv
-import fuel, re
+import fuel, re, planet_list
 
 # Opens the log file and grabs the contents.
 try:
@@ -15,15 +15,9 @@ except IndexError:
 except:
 	exit("Couldn't open file \""+sys.argv[1]+"\".")
 
-# Uncomment and your work in the appropriate spots.
-def Names_of_system_visted(content):
-    pattern = re.compile(r"\"event\":\"FSDJump\", \"StarSystem\":\"(\w+\s\w+[-\s]\w+[-\s]\w+[-\s\w]\"?\w?\w?\w?\w?\s?\w?-?\w?-?\w?\w?)\"?")  
-    matches = pattern.finditer(content)
-    for match in matches:
-        print(match.group(1))
-        
+# Uncomment and your work in the appropriate spots.        
 argSwitcher = {
-	'-s': Names_of_system_visted, #NAMES OF SYSTEMS VISITED        
+	'-s': planet_list.Names_of_system_visted, #NAMES OF SYSTEMS VISITED        
 #	'-p': NAMES OF PLANETS SCANNED
 #	'-t': TOTAL NUMBER OF TERRAFORMABLE PLANETS SCANNED
 #	'-d': TOTAL DISTANCE IN LIGHT YEARS
