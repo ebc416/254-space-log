112ea98d8561b9fbdbe9870bc57ead4322abc58e
Ebc416 <efrainbonilla22@csu.fullerton.edu>
2018-04-05 22:08:13 -0700

    changes have been fixed

--- space_log.py
+++ space_log.py
@@ -3,12 +3,8 @@
 # Use like `./space_log.py -s|-p|-t|-d|-f log_file
 
 from sys import argv
-<<<<<<< HEAD
 import fuel
 import planet_list
-=======
-import fuel, re, planet_list
->>>>>>> 1b1a03a4fdf12e8f4165d9463ba46778e5527e6c
 
 # Opens the log file and grabs the contents.
 try:
@@ -20,15 +16,9 @@ except IndexError:
 except:
 	exit("Couldn't open file \""+argv[1]+"\".")
 
-<<<<<<< HEAD
 # Uncomment, and add your work in the appropriate spots.
 argSwitcher = {
 	'-s': planet_list.Names_of_system_visted,
-=======
-# Uncomment and your work in the appropriate spots.        
-argSwitcher = {
-	'-s': planet_list.Names_of_system_visted, #NAMES OF SYSTEMS VISITED        
->>>>>>> 1b1a03a4fdf12e8f4165d9463ba46778e5527e6c
 #	'-p': NAMES OF PLANETS SCANNED
 #	'-t': TOTAL NUMBER OF TERRAFORMABLE PLANETS SCANNED
 #	'-d': TOTAL DISTANCE IN LIGHT YEARS
@@ -39,15 +29,9 @@ try:
 	func = argSwitcher.get(argv[2], lambda x: "Incorrect search argument.")
 except IndexError:
 	exit("Missing search argument.")
-
-<<<<<<< HEAD
 output = func(content)
 if type(output) is list:
 	for l in output:
 		print(l)
 else:
 	print(output)
-=======
-print(func(content))    
-
->>>>>>> 1b1a03a4fdf12e8f4165d9463ba46778e5527e6c
