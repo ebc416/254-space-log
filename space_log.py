1c0498073d8b7a6ae2b747d5074a5cbf3d0b0949
Ebc416 <efrainbonilla22@csu.fullerton.edu>
2018-04-05 21:58:31 -0700

    new updates and working

--- space_log.py
+++ space_log.py
@@ -4,6 +4,7 @@
 
 from sys import argv
 import fuel
+import planet_list
 
 # Opens the log file and grabs the contents.
 try:
@@ -17,7 +18,7 @@ except:
 
 # Uncomment, and add your work in the appropriate spots.
 argSwitcher = {
-#	'-s': NAMES OF SYSTEMS VISITED
+	'-s': planet_list.Names_of_system_visted,
 #	'-p': NAMES OF PLANETS SCANNED
 #	'-t': TOTAL NUMBER OF TERRAFORMABLE PLANETS SCANNED
 #	'-d': TOTAL DISTANCE IN LIGHT YEARS
