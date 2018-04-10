e5630673587e76c1748217acb185e4ec237896e3
EricMinh <32127944+EricMinh@users.noreply.github.com>
2018-04-09 21:19:50 -0700

    Clarification comment added

--- total_distance.py
+++ total_distance.py
@@ -1,6 +1,8 @@
 #Eric Minh Hanh Nguyen
 #Yeah, I noticed once I was done, it was eeriely similiar to your own. Took me a lot longer to figure out how to add the numbers together
 #than I'd like to admit.
+#P.S. I literally took the fuel.py and put in the pieces where mine was different because I was curious how closely similiar they were.
+#SPENT 1.5-2 HOURS FOR SOMETHING THAT WAS JUST ABOUT THE SAME, but yeah, doing it blind was nice, good times.
 import re
 
 def get_total_distance(content):
