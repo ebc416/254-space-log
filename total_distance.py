bec12742ee0175faf4167836072f216deb81e41f
EricMinh <32127944+EricMinh@users.noreply.github.com>
2018-04-09 18:45:24 -0700

    Update total_distance.py

--- total_distance.py
+++ total_distance.py
@@ -1,7 +1,7 @@
 import re
 
 def get_total_distance(content):
-	pattern = re.compile("\"FuelUsed\":(\d+\.\d+)")
+	pattern = re.compile("\"JumpDist\":(\d+\.\d+)")
 	result = pattern.findall(content)
 	distance = 0
 	if result:
